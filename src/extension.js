4449dbd65208e84f20370b5419a0a584af7f04d7
Mark Hahn <mark@hahnca.com>
2025-03-06 14:39:34 -0800

    clnup

--- src/extension.js
+++ src/extension.js
@@ -1,32 +1,33 @@
 const vscode = require('vscode');
 const utils  = require('./utils.js');
 const log    = utils.getLog('extens');
-// const sym    = require('./symbols.js');
 const webv   = require('./webview.js');
 
 function activate(context) {
-	log("definition-stack activated");
+  log("definition-stack activated");
   
-	const openWebViewCmd = 
-    vscode.commands.registerCommand(
-     'definition-stack.openwebview', 
-      async function() {
-        await webv.openWebView(context);
-        context.subscriptions.push(openWebViewCmd);
-        // sym.processBlocks();
-        log('web view opened');  
-      }	
+  const openWebViewCmd = vscode.commands.registerCommand(
+    'definition-stack.openwebview', 
+    () => {
+      try {
+        webv.openWebView(context);
+        log('web view opened');
+      } catch (error) {
+        log('error opening webview:', error);
+      }
+    }
+  );
+  
+  const closeWebViewCmd = vscode.commands.registerCommand(
+    'definition-stack.closewebview', 
+    () => {
+      webv.closeWebView();
+      log('web view closed');  
+    }
   );
 
-  const closeWebViewCmd = 
-    vscode.commands.registerCommand(
-     'definition-stack.closewebview', 
-      async function() {
-        await webv.closeWebView();
-        context.subscriptions.push(closeWebViewCmd);
-        log('web view closed');  
-      }
-  );
+  context.subscriptions.push(openWebViewCmd, closeWebViewCmd);
+  log("commands registered");
 }
 
 function deactivate() {}
