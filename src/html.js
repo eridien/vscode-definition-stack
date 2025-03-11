c5b57b6af3f2d07d42625435aaeef13823d5a369
Mark Hahn <mark@hahnca.com>
2025-03-10 22:05:15 -0700

    removed html.js comments

--- src/html.js
+++ src/html.js
@@ -141,16 +141,16 @@ function getPageTemplate() { return `
           // Receive a message from anywhere
           window.addEventListener('message', event => {
             const message = event.data;
-            console.log('webview received message:', message);
+            // console.log('webview received message:', message);
             if(message.src === 'extension') {
-              console.log('Received message from extension:', message);
+              // console.log('Received message from extension:', message);
               // post the message to the iframe
               message.src = 'webview';
               iframe.contentWindow.postMessage(message, '*');
               return;
             }
             if(message.src === 'iframe') {
-              console.log('Received message from iframe:', message);
+              // console.log('Received message from iframe:', message);
               // post the message to the extension
               message.src = 'webview';
               vscode.postMessage(message);
