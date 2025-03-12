41dde10cffd2a025f50041db731588c4dfa33c7a
Mark Hahn <mark@hahnca.com>
2025-03-11 21:56:51 -0700

    fixed missing highlighting

--- src/def-stk-script.js
+++ src/def-stk-script.js
@@ -17,16 +17,19 @@
   });
 
   function addPre(html, language) {
-    // console.log('addPre2', {html, language});
     const tempDiv     = document.createElement('div');
     tempDiv.innerHTML = html;
-    const preEle      = tempDiv.firstChild;
+    const preEle      = tempDiv.firstElementChild;
+    const codeEle     = preEle.firstElementChild
     const klass       = `language-${language}`;
-    if(language) preEle.classList.add(klass);
+    if(language) {
+      preEle.classList.add(klass);
+      codeEle.classList.add(klass);
+    }
     document.body.appendChild(preEle);
-    // console.log('before highlightAll', {Prism, preEle, klass, highlightAll: Prism.highlightAll});
     Prism.highlightAll();
-    document.querySelector(`.${klass}`).classList.remove(klass);
+    preEle.classList.remove(klass);
+    codeEle.classList.remove(klass);
   }
 
   function recv(command, data) {
